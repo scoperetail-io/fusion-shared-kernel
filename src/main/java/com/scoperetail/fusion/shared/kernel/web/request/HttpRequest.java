142ead4820f4e7ac4c4d4b9d38a388a3d3a4f8c8
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-08-27 10:59:07 +0530

    REST Inbound message creation and posting to auditor queue implementation.

--- src/main/java/com/scoperetail/fusion/shared/kernel/web/request/HttpRequest.java
+++ src/main/java/com/scoperetail/fusion/shared/kernel/web/request/HttpRequest.java
@@ -1,9 +1,8 @@
-/* ScopeRetail (C)2021 */
-package com.scoperetail.fusion.shared.kernel.common.annotation;
+package com.scoperetail.fusion.shared.kernel.web.request;
 
 /*-
  * *****
- * fusion-shared-kernel
+ * fusion-core
  * -----
  * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
  * -----
@@ -13,10 +12,10 @@ package com.scoperetail.fusion.shared.kernel.common.annotation;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,27 +26,20 @@ package com.scoperetail.fusion.shared.kernel.common.annotation;
  * =====
  */
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import org.springframework.core.annotation.AliasFor;
-import org.springframework.stereotype.Component;
-
-@Documented
-@Retention(RUNTIME)
-@Target(TYPE)
-@Component
-public @interface MessagingAdapter {
-
-  /**
-   * The value may indicate a suggestion for a logical component name, to be turned into a Spring
-   * bean in case of an auto detected component.
-   *
-   * @return the suggested component name, if any (or empty String otherwise)
-   */
-  @AliasFor(annotation = Component.class)
-  String value() default "";
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HttpRequest {
+  private String url;
+  private Map<String, String> queryParams;
+  private String methodType;
+  private Map<String, String> httpHeaders;
+  private String requestBody;
 }
